13a4e192ec7c39fa54dbfde87dc62718980d6feb
Alanas Plascinskas <alanas.pla@gmail.com>
2018-07-03 15:34:53 +0300

    Test for deprecated exported variable added

---
+++ testsuite/tests/rename/should_compile/Rn069_A.hs
@@ -0,0 +1,11 @@
+module Rn069_A(
+  {-# DEPRECATED "func1 export is deprecated" #-}
+  func1
+) where
+
+
+data T = C | D
+
+func1 :: Int
+func1 = 42
+
---
+++ testsuite/tests/rename/should_compile/rn069.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -fwarn-warnings-deprecations #-}
+
+-- Test deprecation of variable and data constructor exports
+
+module ShouldCompile where
+
+import Rn069_A
+
+foo = func1
